53c6eff2cac763d6dd18260f38de90f0dc62a52e
EduSerodio <dudu9090@outlook.com.br>
2023-10-06 19:26:46 -0300

    Implementação dos testes automatizados

--- blogpessoal/Program.cs
+++ blogpessoal/Program.cs
@@ -10,99 +10,105 @@ using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-//Configuração do CORS  - NEW
-builder.Services.AddCors(options =>
-{   
-    options.AddPolicy(name: "MyPolicy",
-    policy =>
+namespace blogpessoal
+{
+    public class Program
     {
-        policy.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    } );
-});
-
-
-// Add services to the container.
-//configuração para não criar um loop infinito no JASON quando fizermos requisição
-builder.Services.AddControllers()
-    .AddNewtonsoftJson(
-        Options =>
+        public static void Main(string[] args)
         {
-            Options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-        }
-    );
+            var builder = WebApplication.CreateBuilder(args);
 
-// CONEXAO COM O BANCO DE DADOS - NEW
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext> (options => options.UseSqlServer(connectionString));
 
-// registrar validações do banco de dados -NEW
-builder.Services.AddTransient<IValidator<Postagem>, PostagemValidator>();
-builder.Services.AddTransient<IValidator<Tema>, TemaValidator>();
-builder.Services.AddTransient<IValidator<User>, UserValidator>();
-
-//Registrar as classes de serviço (SERVICE)
-builder.Services.AddScoped<IPostagemService, PostagemService> ();
-builder.Services.AddScoped<ITemaService, TemaService> ();
-builder.Services.AddScoped<IUserService, UserService> ();
-builder.Services.AddScoped<IAuthService, AuthService> ();
-
-builder.Services.AddAuthentication(options =>
+            //Configuração do CORS  - NEW
+            builder.Services.AddCors(options =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                var key = Encoding.UTF8.GetBytes(Settings.Secret);
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
+                options.AddPolicy(name: "MyPolicy",
+                policy =>
                 {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key)
-                };
+                    policy.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
             });
+            // Add services to the container.
+            //configuração para não criar um loop infinito no JASON quando fizermos requisição
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson(
+                    Options =>
+                    {
+                        Options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                        Options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                    }
+                );
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+            // CONEXAO COM O BANCO DE DADOS - NEW
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddSwaggerGen();
+            // registrar validações do banco de dados -NEW
+            builder.Services.AddTransient<IValidator<Postagem>, PostagemValidator>();
+            builder.Services.AddTransient<IValidator<Tema>, TemaValidator>();
+            builder.Services.AddTransient<IValidator<User>, UserValidator>();
 
-var app = builder.Build();
+            //Registrar as classes de serviço (SERVICE)
+            builder.Services.AddScoped<IPostagemService, PostagemService>();
+            builder.Services.AddScoped<ITemaService, TemaService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
 
-//criar o banco de dados e as tabelas
-using (var scope = app.Services.CreateAsyncScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.EnsureCreated();
+            builder.Services.AddAuthentication(options =>
+                        {
+                            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                        }).AddJwtBearer(options =>
+                        {
+                            var key = Encoding.UTF8.GetBytes(Settings.Secret);
+                            options.SaveToken = true;
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuer = false,
+                                ValidateAudience = false,
+                                ValidateLifetime = true,
+                                ValidateIssuerSigningKey = true,
+                                IssuerSigningKey = new SymmetricSecurityKey(key)
+                            };
+                        });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            //criar o banco de dados e as tabelas
+            using (var scope = app.Services.CreateAsyncScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+            }
+
+            app.UseHttpsRedirection();
+
+            //Inicializa o CORS  - NEW
+            app.UseCors("MyPolicy");
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-
-    app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
-}
-
-app.UseHttpsRedirection();
-
-//Inicializa o CORS  - NEW
-app.UseCors("MyPolicy");
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
